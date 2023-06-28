319f26a49dfd6bf12b547ac11085eb03bbca1cfe
deeba <deeban@gmail.com>
2023-06-28 19:04:35 +0530

    End of Secetion -3

--- API/Program.cs
+++ API/Program.cs
@@ -10,11 +10,12 @@ builder.Services.AddDbContext<DataContext>(opt=>{  //We provide method with opti
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
-
+builder.Services.AddCors();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
+// app.UseCors(builder=>builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
 app.MapControllers();
 
 app.Run();
