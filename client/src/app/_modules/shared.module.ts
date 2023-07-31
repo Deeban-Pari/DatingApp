2b05ffdc2d74867461acbf47575060c0a41c7fb1
Deeban-Pari <deeban.pari@geminisolutions.com>
2023-07-31 19:02:25 +0530

    End of section 8

--- client/src/app/_modules/shared.module.ts
+++ client/src/app/_modules/shared.module.ts
@@ -2,16 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatSelectModule } from '@angular/material/select';
 import { ToastrModule } from 'ngx-toastr';
+import { MatTabsModule } from '@angular/material/tabs';
+import { NgxGalleryModule } from '@kolkov/ngx-gallery';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     MatSelectModule,
+    MatTabsModule,
     ToastrModule.forRoot({
       positionClass: 'toast-bottom-right',
     }),
+    NgxGalleryModule,
   ],
-  exports: [MatSelectModule, ToastrModule],
+  exports: [MatSelectModule, ToastrModule, MatTabsModule, NgxGalleryModule],
 })
 export class SharedModule {}
